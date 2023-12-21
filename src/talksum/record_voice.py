a4f7bc132f1b5426574474544df66a53bf7ab632
Bryan Galvin <bcgalvin@gmail.com>
2023-12-21 14:58:34 -0800

    aider: Refactored record_audio function to use sounddevice for recording and saving audio to a temporary WAV file.
    # Aider chat conversation:
    
    ASSISTANT: No changes are needed.

--- src/talksum/record_voice.py
+++ src/talksum/record_voice.py
@@ -1,9 +1,22 @@
-# This script would contain the logic to record audio from the microphone.
-# As the implementation details are not provided in the README.md, this is a placeholder.
+import sounddevice as sd
+import wave
+import tempfile
 
-def record_audio():
-    # Placeholder function to simulate recording audio and saving to a file.
-    # In a real-world scenario, you would use a library like sounddevice or pyaudio to record audio.
-    audio_path = "path/to/recorded_audio.wav"
-    # Code to record audio and save to the specified path would go here.
+def record_audio(duration=5, sample_rate=44100, channels=2):
+    print(f"Recording for {duration} seconds...")
+    audio_data = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=channels)
+    sd.wait()  # Wait until recording is finished
+
+    # Create a temporary file to store the recording
+    temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.wav', prefix='recorded_')
+    audio_path = temp_file.name
+
+    # Save the recording in a WAV file
+    with wave.open(audio_path, 'wb') as wf:
+        wf.setnchannels(channels)
+        wf.setsampwidth(sd.default.dtype['int16'].itemsize)  # Size of data
+        wf.setframerate(sample_rate)
+        wf.writeframes(audio_data.tobytes())
+
+    print(f"Recording finished: {audio_path}")
     return audio_path
